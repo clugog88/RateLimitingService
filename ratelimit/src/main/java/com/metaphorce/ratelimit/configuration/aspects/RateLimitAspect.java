4608c5247d7935dfc4f1b15bdfdf714134289a51
DESKTOP-L8AAU42\clugo <clugo.g@gmail.com>
2024-07-16 20:11:37 -0600

    Rate limit by roles - Part 2

--- ratelimit/src/main/java/com/metaphorce/ratelimit/configuration/aspects/RateLimitAspect.java
+++ ratelimit/src/main/java/com/metaphorce/ratelimit/configuration/aspects/RateLimitAspect.java
@@ -1,20 +1,21 @@
 package com.metaphorce.ratelimit.configuration.aspects;
 
-//import java.util.ArrayList;
+import java.util.ArrayList;
 import java.util.Collection;
-//import java.util.List;
-//import java.util.concurrent.ConcurrentHashMap;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
-//import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
-//import com.metaphorce.ratelimit.configuration.properties.RateLimitProperties;
+import com.metaphorce.ratelimit.configuration.properties.RateLimitProperties;
+import com.metaphorce.ratelimit.error.exceptions.TooManyRequestsException;
 import com.metaphorce.ratelimit.persistence.entities.enums.UserRoleEnum;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -27,10 +28,10 @@ import jakarta.servlet.http.HttpServletRequest;
 @Component
 public class RateLimitAspect {
 
-//    private final ConcurrentHashMap<String, List<Long>> requestCounts = new ConcurrentHashMap<>();
-//
-//    @Autowired
-//	private RateLimitProperties rateLimitProperties;
+    private final ConcurrentHashMap<String, List<Long>> requestCounts = new ConcurrentHashMap<>();
+
+    @Autowired
+	private RateLimitProperties rateLimitProperties;
     
     @Before("@annotation(com.metaphorce.ratelimit.security.interfaces.WithRateLimitProtection)")
     public void rateLimit() {
@@ -49,25 +50,49 @@ public class RateLimitAspect {
         	hasRegular = rolesList.contains( UserRoleEnum.REGULAR );
         }
         
-//        final String key = httpRequest.getRemoteAddr();
+        final String key = httpRequest.getRemoteAddr();
+        requestCounts.putIfAbsent(key, new ArrayList<>());
         
-//        final long currentTime = System.currentTimeMillis();
-//        requestCounts.putIfAbsent(key, new ArrayList<>());
-//        requestCounts.get(key).add(currentTime);
-//        cleanUpRequestCounts(currentTime);
+        final long currentTime = System.currentTimeMillis();
+        requestCounts.get(key).add(currentTime);
         
-//        if (requestCounts.get(key).size() > rateLimitProperties.getRateLimit() ) {
-//            throw new TooManyRequestsException();
-//        }
+        requestCountCleanup(currentTime);
+        
+        if(hasPremium || hasRegular) {
+        	int requestNum = requestCounts.get(key).size();
+        	if(hasPremium) {
+            	if (requestNum > rateLimitProperties.getRateLimitForPremiumRole() ) {
+                    throw new TooManyRequestsException();
+                }
+            }
+            else if (requestNum > rateLimitProperties.getRateLimitForRegularRole() ) {
+                throw new TooManyRequestsException();
+            }
+        }
+        else {
+        	throw new TooManyRequestsException();
+        }
+        
     }
 
-//    private void cleanUpRequestCounts(final long currentTime) {
-//        requestCounts.values().forEach(l -> {
-//            l.removeIf(t -> timeIsTooOld(currentTime, t));
-//        });
-//    }
+    /**
+     * Request Count Cleanup
+     * @param currentTime Current time to compare.
+     */
+    private void requestCountCleanup(final long currentTime) {
+        requestCounts.values().forEach(timesList -> {
+        	timesList.removeIf(time -> timeIsTooOld(currentTime, time));
+        });
+    }
 
-//    private boolean timeIsTooOld(final long currentTime, final long timeToCheck) {
-//        return (currentTime - timeToCheck) > rateLimitProperties.getRateDuration();
-//    }
+    /**
+     * Evaluates if the saved time is within the duration configured as a limit.
+     * @param currentTime Current time to compare.
+     * @param timeToCheck Time to evaluate.
+     * @return Evaluation result.
+     */
+    private boolean timeIsTooOld(final long currentTime, final long timeToCheck) {
+        return (currentTime - timeToCheck) > rateLimitProperties.getRateDuration();
+    }
+    
 }
